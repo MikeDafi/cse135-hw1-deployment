3124052427c3bdcdce53965099bb66939ec1d094
Michael Askndafi <maskndafi@gmail.com>
2021-08-09 15:26:42 -0700

    changing favicon/new404 locations

--- var/www/michaelaskndafi.site/public_html/hw1/hello.php
+++ var/www/michaelaskndafi.site/public_html/hw1/hello.php
@@ -1,9 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<link rel="icon" 
-          type="image/png" 
-          href="https://www.pngfind.com/pngs/m/20-205207_link-png-transparent-png.png">
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Hello PHP</title>
